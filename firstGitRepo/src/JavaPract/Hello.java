e6925ffa67bc11cf02b6506efc3b4c0cffc371d7
arvin <arvin@192.168.0.19>
2018-07-05 14:10:01 -0400

    for loop added

--- firstGitRepo/src/JavaPract/Hello.java
+++ firstGitRepo/src/JavaPract/Hello.java
@@ -6,6 +6,22 @@ public class Hello {
 		// TODO Auto-generated method stub
 System.out.println("hello World");
 System.out.println("new line added");
+int x = 0;
+int y =0;
+for(x=1; x<=10; x++){
+	if(x<5){
+	y=y-x;
+	System.out.println("y value is "+y);
+	}else if(x>5){
+		y =y+x;
+		System.out.println("y value is"+y);
+	}else{
+		y= y-x;
+		System.out.println("y value is "+y);
+	}
+		
+	
+}
 
 
 	}
