e530d86407aa25d33c0456ac91d16f5c76835de2
arvin <arvin@192.168.0.19>
2018-07-05 11:51:49 -0400

    new line added

--- firstGitRepo/src/JavaPract/Hello.java
+++ firstGitRepo/src/JavaPract/Hello.java
@@ -4,7 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("hello World");
+System.out.println("hello World");
+System.out.println("new line added");
+
+
 	}
 
 }
